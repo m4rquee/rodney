69e53372aa928600851499104b0b365ed066dcbc
LucasXM12 <lucasxm21@hotmail.com>
2017-10-27 10:46:53 -0200

    L function

--- Rodney/src/main/java/com/marquee/rodney/Functions.java
+++ Rodney/src/main/java/com/marquee/rodney/Functions.java
@@ -2,4 +2,10 @@ package com.marquee.rodney;
 
 public class Functions {
 
+    public static double huberLoss() {
+    }
+
+    public static double LFunction(double δ) {
+        return Math.abs(δ) <= 1 ? Math.pow(δ, 2) / 2 : δ - 1 / 2;
+    }
 }
