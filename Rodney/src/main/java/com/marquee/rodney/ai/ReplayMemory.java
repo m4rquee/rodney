399b8adb44b13112ff8dbe198c1c239f37104a19
LucasXM12 <lucasxm21@hotmail.com>
2017-10-27 10:38:11 -0200

    Transition class

--- Rodney/src/main/java/com/marquee/rodney/ai/ReplayMemory.java
+++ Rodney/src/main/java/com/marquee/rodney/ai/ReplayMemory.java
@@ -3,12 +3,12 @@ package com.marquee.rodney.ai;
 import java.util.*;
 import java.util.stream.*;
 
-public class ReplayMemory<T> {
+public class ReplayMemory {
 
     private final int capacity;
     private int position;
 
-    private final ArrayList<T> transitions;
+    private final ArrayList<Transition> transitions;
 
     public ReplayMemory(int capacity) throws IllegalArgumentException {
         this.transitions = new ArrayList(capacity);
@@ -16,7 +16,7 @@ public class ReplayMemory<T> {
         this.position = 0;
     }
 
-    public void push(T transition) {
+    public void push(Transition transition) {
         if (this.transitions.size() < this.capacity) {
             this.transitions.add(transition);
         } else {
@@ -26,8 +26,8 @@ public class ReplayMemory<T> {
         this.position = ++this.position % this.capacity;
     }
 
-    public ArrayList<T> sample(int batchSize) throws IllegalArgumentException {
-        if (batchSize < 0) {
+    public ArrayList<Transition> sample(int batchSize) throws IllegalArgumentException {
+        if (batchSize < 0 || batchSize > this.capacity) {
             throw new IllegalArgumentException("Illegal size: " + batchSize);
         }
 
---
+++ Rodney/src/main/java/com/marquee/rodney/ai/Transition.java
@@ -0,0 +1,10 @@
+package com.marquee.rodney.ai;
+
+public class Transition {
+
+    private Object state;
+    private Object action;
+    private Object nextState;
+
+    private double reward;
+}
